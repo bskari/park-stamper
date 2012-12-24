db1086036a2940165465d34fa9c6e0b1c51a2d9e
Brandon Skari <brandon@skari.org>
2012-12-23 20:15:11 -0800

    Full width to show non-wrapped stamp text

--- parks/views/all_parks.py
+++ parks/views/all_parks.py
@@ -1,13 +1,18 @@
+from collections import namedtuple
 from pyramid.view import view_config
 
 from parks.models import DBSession
 from parks.models import Park
 
 
-@view_config(route_name='all_parks', renderer='all_parks.mako')
+@view_config(route_name='all-parks', renderer='all_parks.mako')
 def all_parks(request):
     parks = DBSession.query(
-        Park,
+        Park.name,
+        Park.region,
+        Park.latitude,
+        Park.longitude,
     ).all()
+    ParkTuple = namedtuple('ParkTuple', ['name', 'region', 'latitude', 'longitude'])
 
     return dict(parks=parks)
